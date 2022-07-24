31a3ee8fb963a4e54e6660061ba7d33a440d296a
Matt Yale <VhatAmI@users.noreply.github.com>
2022-07-24 11:41:17 -0500

    initial project commit
    Confluent.Kafka only non-Microsoft dependency

---
+++ Meth/Meth/MethProducer.cs
@@ -0,0 +1,196 @@
+﻿//Matt Yale 2022 Feb
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Meth
+{
+
+    //NewProducer(kafkaBrokerURL, defaultTopic string, schema map[string] string) 
+    // changing to dictionary data type, key value pairs
+
+    //AddBlock(number int64, hash, parentHash types.Hash, weight *big.Int, 
+    // updates map[string][] byte, deletes map[string]struct{ }, batches map
+    //[string]types.Hash)
+
+    /*
+    BlockHash: A 32 byte identifier unique to this message payload. --unique
+    Number: A 64 bit unsigned integer indicating the sequence number of this payload.
+    Parent: The $BlockHash of the block preceding this one.
+    Weight: A 256 bit unsigned indicator indicating the weight of this payload. If this is not meaningful for a given application, it can duplicate $Number.
+     */
+
+
+    /// Not started yet
+    /// SendBatch(batchid types.Hash, delete []string, update map[string][] byte) (map[string][] Message, error)
+    /// 
+    ///
+    /// Reorg(number int64, hash types.Hash)
+    /// ReorgDone(number int64, hash types.Hash)
+
+    class WrappedMethProducer //custom class wrapping kafka library to add more data to the producer
+    {
+        private IProducer<string, string> _Producer; //Producer is keyword, but internal class inacessible
+        private string Topic;
+        private string ReOrgTopic;
+        private Dictionary<string, string> SchemaMap;
+        private string brokerURL; //expose to constructor
+
+        //private Dictionary<int, batchInfo>  pendingBatches;   //Map batchid -> batchInfo -- not sure what type batchInfo is
+        //
+
+
+        //from go impl
+        //type batchInfo struct
+        //{
+        // topic string
+        //  block types.Hash   -- types.Hash equivalent in C#? 32 byte array
+        //}               
+
+        //Encapsolates a kafka producer, C# kafka lib has no way to store default topic in the producer... 
+        public WrappedMethProducer(string kafkaBrokerURL, string topic, Dictionary<string, string> schemaMap, string specialname = null)
+        {
+            var configuration = new ProducerConfig()
+            {
+                BootstrapServers = kafkaBrokerURL, //I think you can add more than one broker or server to a producer, will add that as an auxilary method
+                ClientId = specialname ?? "Meth2022.V1", //might come in use if this continues evolving, telling where the messages came from
+                MessageTimeoutMs = 30000, //keeps flush from blocking more than 10 seconds, just to make testing easier when hitting server urls that dont exist
+                EnableDeliveryReports = true,
+                MessageSendMaxRetries = 3,
+                //other config things?                
+            };
+
+            _Producer = new ProducerBuilder<string, string>(configuration.AsEnumerable()).Build();
+            Topic = topic;
+            SchemaMap = schemaMap;
+            brokerURL = kafkaBrokerURL;
+        }
+
+        public void AddBroker(string newServer)
+        {
+            _Producer.AddBrokers(newServer);
+        }
+
+        /// <summary>
+        ///  Add Block Meth API call
+        /// </summary>
+        /// <param name="number"> plain int example 1337</param>
+        /// <param name="hash"> Example : 0xdeadbeef0123456789abcdef00000000000000fedcba987654321fffffffffff hexidecimal hash?</param>
+        /// <param name="parentHash"> Example: 0x0000000000000000000000000000000000000000000000000000000000000000 hexidecimal hash? </param>
+        /// <param name="weight"> 0x2674</param>
+        /// <param name="updates"> Example :  { "a/b": 0x88, "q/17": 0x1234, "q/18": 0x5678 } </param>
+        /// <param name="deletes"> Examples :   ["b/c"] or ["b/c", "t/g"] </param>
+        /// <param name="batches"> Example: Subbatches: {"b/s": 0x0000000000000000000000000000000000000000000000000000000000000001} Subbatch details: Updates: { "b/s/5": 0xabcd } Deletes: ["b/s/4"]</param>
+        ///    batches param type might need tweeking since there are subbatches? 
+        /// <returns></returns>
+        public async Task AddBlock(int number, byte[] hash, byte[] parentHash, byte[] weight, Dictionary<string, byte[]> updates, List<string> deletes, Dictionary<string, byte[]> batches )
+        {
+            Console.WriteLine("Adding Block for topic " + Topic + " and producer " + _Producer.Name);
+
+            //create messages
+
+            //avro encode
+
+            //send
+
+
+            //For each match create message, encode, and send (Avro Encoded)
+
+            var message = new Message<string, string>(); //is this supposed to be the schema map?
+                                                         //pretty sure this is what kafka sends, need to nail down what structure this is
+                                                         //has to be created by params somehow, a real example would likely be helpful 
+            message.Key = "Key-7-20---Roy";
+            message.Value = "Test7-20---Roy";
+            //change to Produce Async, capture errors and log them --- 
+
+            //have this be a new Task on it's own thread-- that way things dont get blocked
+            try
+            {
+                var deliveryResult = await _Producer.ProduceAsync(Topic, message);
+
+                Console.WriteLine("Delivery result status :" + deliveryResult.Status);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ProduceAsync threw an exception : Short Message " + ex.Message + "\n Long Message : " + ex.StackTrace);
+            }
+            return;
+        }
+
+        public void ReOrg()
+        {
+            //notify that a bunch of blocks coming with the reorg
+            //addblock for all new blocks
+            //send ReOrgDone
+        }
+
+        public void ReOrgDone()
+        {
+
+        }
+
+        public void Flush()
+        {
+            _Producer.Flush();
+        }
+        //methods here would be called on instances of the class
+        //addblock -- has access to the topic and schema map, unlike vanilla kafka lib
+        //reorg etc etc
+    }
+
+
+    //just for reference, below code no longer needed to be used
+    class MethProducer //using only kafka library -- just used for testing and figuring out the lib
+    {
+        //not asked for in spec doc, could be useful though
+        public static void AddBroker(IProducer<string, string> theProducer, string newServer)
+        {
+            theProducer.AddBrokers(newServer); //does not look like there is a way to remove brokers, only add new ones
+        }
+
+        // not void, not sure of return type yet, likely custom type, unsure of param types also, need to look at go implementation 
+        public static void AddBlock(int hash, IProducer<string, string> theProducer, string topic)
+        {
+            Console.WriteLine("Adding Block for topic " + topic + " and producer " + theProducer.Name);
+            //there is much more to this... for now just testing
+            //a little different, pass the producer you want to AddBlock, need more param clarification 
+            //can have an AddBlock that takes a list of Producers as well if needed to add a block to multiple producers
+            var message = new Message<string, string>(); //is this supposed to be the schema map?
+            theProducer.Produce(topic, message);
+            return;
+        }
+
+        //topic and schema map params to be removed once confirming with austin current strategy ok. 
+        //problem, C# kafka library has no way to set default topic in the producer
+        public static IProducer<string, string> NewProducer(string kafkaBrokerURL, string topic, Dictionary<string, string> schemaMap, string specialname = null)
+        {
+            //old config attempt
+            //IConfiguration configuration = new ConfigurationBuilder()
+            //    .Build();
+            //                .Properties.Add(topic, schemaMap)
+
+            var configuration = new ProducerConfig()
+            {
+                BootstrapServers = kafkaBrokerURL, //I think you can add more than one broker or server to a producer, will add that as an auxilary method
+                ClientId = specialname ?? "Meth2022.V1", //might come in use if this continues evolving, telling where the messages came from
+                MessageTimeoutMs = 10000, //keeps flush from blocking more than 10 seconds, just to make testing easier when hitting server urls that dont exist
+                //other config things?                
+            };
+
+            //need to figure out how to get topic and schema map params into the config still
+
+            var producer = new ProducerBuilder<string, string>(configuration.AsEnumerable()).Build();
+            //Solved with BootstrapServers in config... 
+            //%5|1652645662.511|CONFWARN|rdkafka#producer-1| [thrd:app]: No `bootstrap.servers` configured: client will not be able to connect to Kafka cluster -- current error when building with this config
+            Console.WriteLine("Producer created");
+            Console.WriteLine("Adding broker " + kafkaBrokerURL);
+
+            return producer;
+        }
+        
+    }
+}
---
+++ Meth/Meth/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Meth;  //will change name and reorganize before any commiting or delivery
+
+Console.WriteLine("Hello, World!");
+
+
+int hashId = 12345;
+Dictionary<string, string> test = new Dictionary<string, string>();
+
+
+//second pass
+Console.WriteLine("Creating Improved producer");
+var improved = new Meth.WrappedMethProducer("b-1.mattbroker.gfrhzv.c6.kafka.us-east-2.amazonaws.com:9092", "interestingTopic", test); // "WrappedLib");
+
+//var improved = new Meth.WrappedMethProducer("z-2.mattbroker.gfrhzv.c6.kafka.us-east-2.amazonaws.com:2181", "interestingTopic", test); // "WrappedLib");
+
+await improved.AddBlock();//currently params undefined
+Thread.Sleep(10000);
+Console.WriteLine("Flushing");
+improved.Flush();
+Console.WriteLine("Flush complete");
+
+Console.WriteLine("Press any key to close");
+Console.ReadKey();
+return;//end program, don't run first pass code
+
+//first pass below --this code wont run currently
+//right now an empty dictionary
+Console.WriteLine("Creating Walter White");
+var WalterWhite = Meth.MethProducer.NewProducer("URL", "none", test, "WalterWhite");
+
+Console.WriteLine("Creating Jesse Pinkman");
+var JessePinkman = Meth.MethProducer.NewProducer("URL44", "othertopics", test, "JessePinkman");
+
+//Meth.MethProducer.AddBroker(WalterWhite, "URL2");
+
+Meth.MethProducer.AddBlock(hashId, WalterWhite, "TopicOfTheDay");  //incomplete method AddBlock, need to look more at Go implementation
+Meth.MethProducer.AddBlock(hashId, JessePinkman, "DifferentTopic");
+
+//void Produce(string topic, Message<TKey, TValue> message, Action<DeliveryReport<TKey, TValue>> deliveryHandler = null);
+//void Produce(TopicPartition topicPartition, Message<TKey, TValue> message, Action<DeliveryReport<TKey, TValue>> deliveryHandler = null);
+//WalterWhite.Produce() two ways to send a single message with the producer
+//WalterWhite.ProduceAsync() two ways to do it async
+
+Console.WriteLine("Flushing");
+WalterWhite.Flush();
+Console.WriteLine("Flush complete");
+
+Console.WriteLine("Press any key to close");
+Console.ReadKey();
