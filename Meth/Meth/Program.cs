4146d5f456bed9ad569ed532235641bd3e24a010
Matt Yale <VhatAmI@users.noreply.github.com>
2022-07-24 12:32:17 -0500

    Change Addblock call in sample to take in actual example params
    Still have questions about batches, left it empty for now

--- Meth/Meth/Program.cs
+++ Meth/Meth/Program.cs
@@ -14,7 +14,28 @@ var improved = new Meth.WrappedMethProducer("b-1.mattbroker.gfrhzv.c6.kafka.us-e
 
 //var improved = new Meth.WrappedMethProducer("z-2.mattbroker.gfrhzv.c6.kafka.us-east-2.amazonaws.com:2181", "interestingTopic", test); // "WrappedLib");
 
-await improved.AddBlock();//currently params undefined
+
+//string hashString = "0xdeadbeef0123456789abcdef00000000000000fedcba987654321fffffffffff";
+
+byte[] hash = new byte[] { 0xde, 0xad, 0xbe, 0xef, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x1f, 0xff, 0xff, 0xff, 0xff, 0xff };
+byte[] parent = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+byte[] weight = new byte[] { 0x26, 0x74 }; 
+
+
+//there is probably a better way to deal with byte[] in C#, dont often use byte array as a data type
+Dictionary<string, byte[]> updates = new Dictionary<string, byte[]>(){
+    { "a/b", new byte[] {0x88} },
+    {"q/17", new byte[] {0x12, 0x34} },
+    { "q/18", new byte[] {0x56, 0x78 } }
+};
+
+var deletes = new List<string>() { "b/c" }; //["b/c"] for single, multiple ["b/c", "t/g"]
+Dictionary<string, byte[]> batches = new Dictionary<string, byte[]>() { };// empty for now... need to figure out batches datatype, since it has subbatches
+
+//call add block with the variables from the example documentation
+await improved.AddBlock(1337, hash, parent, weight, updates, deletes, batches );//currently params undefined
+
+
 Thread.Sleep(10000);
 Console.WriteLine("Flushing");
 improved.Flush();
